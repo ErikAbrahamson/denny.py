b47d5744dd207f846c525242cb1d788dee371482
ErikAbrahamson <e.a.abrahamson@gmail.com>
2020-03-21 15:23:37 -0600

    add training in for non-cuda

--- denny.py
+++ denny.py
@@ -12,7 +12,6 @@ IMGFLIP_PASSWORD = os.getenv('IMGFLIP_PASSWORD')
 TOKEN = os.getenv('DENNY_DISCORD_TOKEN')
 
 ENV_HAS_CUDA = os.name == 'nt'
-BATCH_SIZE = 1024 if ENV_HAS_CUDA else 128
 MODEL_DIR = os.getcwd() + '/model/v1/'
 client = discord.Client()
 
@@ -55,23 +54,44 @@ class DennyClient(discord.Client):
             return
         await message.channel.send(self.generate_response())
 
-    def train(self):
-        self.model.train_on_texts(msgs,
-                                  max_length=10,
-                                  new_model=False,
-                                  batch_size=BATCH_SIZE,
-                                  num_epochs=100,
-                                  gen_epoch=50)
+    async def train(self, message):
+        try:
+            history = await message.channel.history(limit=20000).flatten()
+            messages = []
 
-        self.model.save(MODEL_DIR + 'denny_weights.hdf5')
-        print(self.model.model.summary())
+            for m in history:
+                line = m.content
+                not_author = 'Denny' not in m.author.name
+                not_referenced = 'denny' not in m.content.lower()
+
+                if not_author and not_referenced:
+                    messages.append(line)
+
+            num_epochs = 100 if ENV_HAS_CUDA else 5
+            batch_size = 1024 if ENV_HAS_CUDA else 128
+            gen_epoch = 50 if ENV_HAS_CUDA else 1
+
+            self.model.train_on_texts(messages,
+                                      max_length=10,
+                                      new_model=False,
+                                      batch_size=batch_size,
+                                      num_epochs=num_epochs,
+                                      gen_epoch=gen_epoch)
+
+            self.model.save(MODEL_DIR + 'denny_weights.hdf5')
+            print(self.model.model.summary())
+
+        except discord.HTTPException:
+            # TODO: Save history to file periodically
+            return
 
     def generate_response(self, message):
         match = re.findall('(?i)denny', message.content)
         train = re.findall('(?i)homework', message.content)
         meme = re.findall('(?i)meme', message.content)
+
         resp = self.model.generate(1, return_as_list=True,
-                                          temperature=self.default_temp)[0]
+                                   temperature=self.default_temp)[0]
         if match:
             async with message.channel.typing():
                 if meme:
@@ -80,7 +100,11 @@ class DennyClient(discord.Client):
                     self.train()
                     return 'Finished my homework!'
                 else:
-                    if len(resp) == 0:
+                    if len(resp) == 0 or resp.startswith('<'):
                         resp = self.model.generate(1, return_as_list=True,
                                                    temperature=0.5)[0]
                     return resp
+
+
+client = DennyClient(discord.Client())
+client.run(TOKEN)
